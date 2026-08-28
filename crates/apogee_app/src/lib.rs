f59da1a5ab98ac6fe9b7ff2238df81f4a34b25ee
Rémy Fayet <rems64des@gmail.com>
2026-08-28 17:20:07 +0200

    ui: add very basic placeholder ui

--- crates/apogee_app/src/lib.rs
+++ crates/apogee_app/src/lib.rs
@@ -1,16 +1,21 @@
-use gpui::{App, Application, Context, Window, WindowOptions, div, prelude::*, rgb};
+use gpui::{App, Context, Window, WindowOptions, div, prelude::*, px, rgb};
+use gpui_platform::application;
 
 #[derive(Default)]
 struct ApogeeApp {}
 
 impl Render for ApogeeApp {
     fn render(&mut self, _window: &mut Window, _cx: &mut Context<Self>) -> impl IntoElement {
-        div().size_full().flex().bg(rgb(0x888888))
+        div().size_full().flex().flex_row().children([
+            div().w(px(250.0)).h_full().flex_none().bg(rgb(0x00aa00)),
+            div().flex_1().h_full().bg(rgb(0x1e1e2e)),
+            div().w(px(250.0)).h_full().flex_none().bg(rgb(0xaa0000)),
+        ])
     }
 }
 
 pub fn run() {
-    Application::new().run(|cx: &mut App| {
+    application().run(|cx: &mut App| {
         cx.open_window(WindowOptions::default(), |_, cx| {
             cx.new(|_| ApogeeApp::default())
         })
