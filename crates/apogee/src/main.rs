9b984104c0e4ad4fa0c780ebdacaefa88ee86361
Rémy Fayet <rems64des@gmail.com>
2026-08-31 12:02:15 +0200

    chore: add tracing

--- crates/apogee/src/main.rs
+++ crates/apogee/src/main.rs
@@ -1,3 +1,10 @@
+use tracing::info;
+
 fn main() {
+    let subscriber = tracing_subscriber::FmtSubscriber::new();
+    tracing::subscriber::set_global_default(subscriber).expect("Couldn't register the logger");
+
+    info!("Hey!");
+
     apogee_app::run();
 }
